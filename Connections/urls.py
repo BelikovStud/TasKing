114dc970a712a86854b2c4f0930b3b14ed8b333e
Daniel Malky <Danielmy@mta.ac.il>
2021-05-06 23:15:21 +0300

    add

--- Connections/urls.py
+++ Connections/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('my-groups', views.my_groups, name='mygroups'),
     path('group/<group_id>/', views.view_group, name='mygroups'),
     path('assign/<task_id>/', views.assign, name='assign'),
+    path('approve/<task_id>/', views.approve, name='approve'),
 ]
