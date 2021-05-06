11a91a645efd51666c931c094b00331308c4f5c5
Daniel Malky <Danielmy@mta.ac.il>
2021-05-06 15:00:03 +0300

    adding migrations

--- Group/models.py
+++ Group/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator
 
 
 class Group(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='maker', default=User.objects.first())
+    creator = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='maker')
     prize = models.IntegerField(validators=[MaxValueValidator(1000)])
     about = models.TextField()
     
--- Task/models.py
+++ Task/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator
 
 
 class Task(models.Model):
-    assignee = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='assign', default=User.objects.first())
+    assignee = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='assign')
     title = models.CharField(max_length=15)
     description = models.TextField()
     points = models.IntegerField(validators=[MaxValueValidator(10)])
