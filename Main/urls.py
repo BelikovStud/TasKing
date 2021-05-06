c53f915fb523c9a399f983ffd982dfebb2548eb6
Daniel Malky <Danielmy@mta.ac.il>
2021-05-06 19:40:22 +0300

    fix

--- Main/urls.py
+++ Main/urls.py
@@ -3,4 +3,6 @@ from . import views as Main_Views
 
 urlpatterns = [
     path('', Main_Views.home_view, name='home'),
+    path('market/', Main_Views.marketplace, name='market'),
+    path('coming/', Main_Views.coming, name='coming'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'Main/index.html')
+
+def marketplace(request):
+    return render(request, 'Main/marketplace.html')
+
+def coming(request):
+    html = "<html><body>Coming Soon.</body></html>"
+    return HttpResponse(html)
