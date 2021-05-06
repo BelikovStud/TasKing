b5821207d5001194285e6d799363afe1f599dbda
Daniel Malky <Danielmy@mta.ac.il>
2021-05-06 12:53:17 +0300

    hello

--- TasKing/settings.py
+++ TasKing/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Group.apps.GroupConfig',
+    'Task.apps.TaskConfig',
 ]
 
 MIDDLEWARE = [
